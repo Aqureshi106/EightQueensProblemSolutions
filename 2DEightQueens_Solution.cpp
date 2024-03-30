cd3910654f91d7c89b681889a185290819933c1e
Ashir Qureshi <160384665+Aqureshi106@users.noreply.github.com>
2024-03-29 20:42:26 -0400

    Update 2DEightQueens_Solution.cpp

--- 2DEightQueens_Solution.cpp
+++ 2DEightQueens_Solution.cpp
@@ -51,7 +51,7 @@ int main() {
     cout << "Solution #" << ++numSolutions << ":\n";
   for (int r = 0; r < 8; r++) {
     for (int c = 0; c < 8; c++) {
-      cout << q[r][c];
+      cout << q[r][c] << " ";
     }
     cout << endl;
   }
