a2465b14b39812b807a1d76ad12f31218359909e
Ashir Qureshi <160384665+Aqureshi106@users.noreply.github.com>
2024-05-11 15:09:56 -0400

    Update NQueens_Solution.cpp

--- NQueens_Solution.cpp
+++ NQueens_Solution.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 bool ok(int q[], int c) {
     for (int i = 0; i < c; i++) {
-        if (q[c] == q[i] or (c - i) == abs(q[c] - q[i])) {
+        if (q[c] == q[i] or(c - i) == abs(q[c] - q[i])) {
             return false;
         }
     }
@@ -40,8 +40,15 @@ int nqueens(int n) {
 }
 
 int main() {
-    int n = 12;
-    for (int i = 1; i <= n; ++i)
-        cout << "There are " << nqueens(i) << " solutions to the " << i << " queens problem.\n";
+    int n = 0;
+    cout << "Please input a positive integer to find the amount of solutions of n number of queens on n by n chessboard: ";
+    cin >> n;
+    while (n < 0) {
+        if (n < 0) {
+        cout << "Incorrect input! Try again: ";
+        cin >> n;
+        }
+    }
+    cout << "There are " << nqueens(n) << " solutions to the " << n << " queens problem.\n";
     return 0;
 }
